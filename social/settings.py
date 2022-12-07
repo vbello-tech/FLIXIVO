a0fa1eda19621b39ae3573a807ce17535f0f7944
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2022-12-07 00:20:32 -0800

    db

--- social/settings.py
+++ social/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,13 +85,20 @@ WSGI_APPLICATION = 'social.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default':  dj_database_url.config(
+        default='postgres://tzjwjqjs:1c21QA_ibSoj8N1BqiEuWnJyPvdkCJ0D@surus.db.elephantsql.com/tzjwjqjs'
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
