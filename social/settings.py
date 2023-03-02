959a64e9a292c09eda08ccd342bb5e2b444771ff
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2023-03-02 11:29:52 -0800

    cors

--- social/settings.py
+++ social/settings.py
@@ -116,9 +116,9 @@ else:
         'API_SECRET': '8fqL8OfZKqSJChXcObtsRWqefPQ'
     }
 
-    CSRF_TRUSTED_ORIGINS = ['https://twitterclone.fly.dev/']
+CSRF_TRUSTED_ORIGINS = ['https://twitterclone.fly.dev/']
 
-    CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Password validation
